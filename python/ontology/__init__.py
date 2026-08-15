4e3efbdb0bceca317dbe1f6498d21c214702a94b
Jordy Orel KONDA <jordyorel@gmail.com>
2026-08-15 16:43:32 +0100

    refactor: remove Python Ontology Layer and related components

--- python/ontology/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Orus Data Engine - Python Ontology Layer
-Decoupled Business Domain Modeling & Knowledge Graph Layer built on top of unmodified Orus Data Engine (Zig).
-"""
-
-__version__ = "0.1.0"
